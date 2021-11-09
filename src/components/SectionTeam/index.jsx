01b99e9dd03d87d1404f30e305fca3130f13edf9
Natália <nataliallcoelho@gmail.com>
2021-11-09 22:04:49 +0000

    Fotos do time, remoção de react-bootstrap

--- src/components/SectionTeam/index.jsx
+++ src/components/SectionTeam/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import img from '../../assets/time/teste.jpg'
+import alex from '../../assets/img/team/Alex.jpg'
+import ana from '../../assets/img/team/Ana.jpg'
+import liliane from '../../assets/img/team/Liliane.jpg'
+import mavi from '../../assets/img/team/Mavi.jpeg'
+import victor from '../../assets/img/team/Victor.jpg'
+import natalia from '../../assets/img/team/Natália.jpg'
 import './style.scss'
 
 export default function SectionTeam() {
@@ -19,30 +24,30 @@ export default function SectionTeam() {
                                 <div className='col-6 col-md-4 text-center'>
 
                                     <div className='box mx-auto'>
-                                        <img className='w-100' src={img} />
+                                        <img className='w-100' src={alex} />
                                     </div>
                                     
-                                    <p className="mb-0 fs-5 fw-bold">Maria</p>
+                                    <p className="mb-0 fs-5 fw-bold">Alex Alan</p>
                                     <p className="text-muted">Descrition</p>
                                     
                                 </div>
                                 <div className='col-6 col-md-4 text-center'>
 
                                     <div className='box mx-auto'>
-                                        <img className='w-100' src={img} />
+                                        <img className='w-100' src={ana} />
                                     </div>
                                     
-                                    <p className="mb-0 fs-5 fw-bold">Maria</p>
+                                    <p className="mb-0 fs-5 fw-bold">Ana Beatriz</p>
                                     <p className="text-muted">Descrition</p>
 
                                 </div>
                                 <div className='col-6 col-md-4 text-center'>
 
                                     <div className='box mx-auto'>
-                                        <img className='w-100' src={img} />
+                                        <img className='w-100' src={liliane} />
                                     </div>
                                     
-                                    <p className="mb-0 fs-5 fw-bold">Maria</p>
+                                    <p className="mb-0 fs-5 fw-bold">Liliane</p>
                                     <p className="text-muted">Descrition</p>
 
                                 </div>
@@ -52,20 +57,30 @@ export default function SectionTeam() {
                                 <div className='col-6 col-md-4 text-center'>
 
                                     <div className='box mx-auto'>
-                                        <img className='w-100' src={img} />
+                                        <img className='w-100' src={mavi} />
                                     </div>
 
-                                    <p className="mb-0 fs-5 fw-bold">Maria</p>
+                                    <p className="mb-0 fs-5 fw-bold">Maria Vitória</p>
                                     <p className="text-muted">Descrition</p>
 
                                 </div>
                                 <div className='col-6 col-md-4 text-center'>
 
                                     <div className='box mx-auto'>
-                                        <img className='w-100' src={img} />
+                                        <img className='w-100' src={natalia} />
                                     </div>
 
-                                    <p className="mb-0 fs-5 fw-bold">Maria</p>
+                                    <p className="mb-0 fs-5 fw-bold">Natália</p>
+                                    <p className="text-muted">Descrition</p>
+
+                                </div>
+                                <div className='col-6 col-md-4 text-center'>
+
+                                    <div className='box mx-auto'>
+                                        <img className='w-100' src={victor} />
+                                    </div>
+
+                                    <p className="mb-0 fs-5 fw-bold">Victor</p>
                                     <p className="text-muted">Descrition</p>
 
                                 </div>
